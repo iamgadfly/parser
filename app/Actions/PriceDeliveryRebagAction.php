3027aedc5828c79ee191a87d246919b2df15823c
iamgadfly <mironov.m@itcomp.org>
2023-05-29 12:36:44 +0300

    add check errors in function

--- app/Actions/PriceDeliveryRebagAction.php
+++ app/Actions/PriceDeliveryRebagAction.php
@@ -2,14 +2,19 @@
 
 namespace App\Actions;
 
+use Exception;
+
 class PriceDeliveryRebagAction
 {
     public static function priceCalculate($raw_price, $dollar_course, $delivery)
     {
         $price = match (true) {
+            $raw_price > 1500 => new Exception('price cant be more then 1500$', 400),
+            $raw_price < 0 => new Exception('price cant be less then 0$', 400),
             $raw_price <= 500 => $dollar_course * ($raw_price * 1.1) + $dollar_course * $delivery,
             $raw_price > 500 && $raw_price <= 1500 => $dollar_course * ($raw_price * 1.1) + $dollar_course * ($delivery + ($raw_price - 500) * 0.15),
         };
-        return (string) PriceDeliveryAction::priceRound($price, 50);
+
+        return $price instanceof Exception ? $price : PriceDeliveryAction::priceRound($price, 50);
     }
 }
--- tests/Unit/PriceRebagUnitTest.php
+++ tests/Unit/PriceRebagUnitTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Actions\PriceDeliveryRebagAction;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class PriceRebagUnitTest extends TestCase
@@ -10,12 +11,33 @@ class PriceRebagUnitTest extends TestCase
     public function test_price_1()
     {
         $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(499, 75.85, 60), "46150");
-        $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(600, 75, 40), "53600");
     }
 
     public function test_price_2()
     {
-        $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(400, 75, 60), "37500");
         $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(1000, 75, 40), "91100");
     }
+    public function test_price_3()
+    {
+        $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(600, 75, 40), "53600");
+    }
+
+    public function test_price_4()
+    {
+        $this->assertEquals(PriceDeliveryRebagAction::priceCalculate(400, 75, 60), "37500");
+
+    }
+
+    public function test_price_5()
+    {
+        $check = PriceDeliveryRebagAction::priceCalculate(2000, 75, 60) instanceof Exception;
+        $this->assertSame($check, true);
+    }
+
+    public function test_price_6()
+    {
+        $check = PriceDeliveryRebagAction::priceCalculate(-322, 75, 60) instanceof Exception;
+        $this->assertSame($check, true);
+    }
+
 }
